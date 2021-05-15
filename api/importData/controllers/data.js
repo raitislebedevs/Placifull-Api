3d487447d90f346ec60868e5b73c75f73ca386c2
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-05-15 12:36:23 +0300

    API-NoFail

---
+++ api/importData/controllers/data.js
@@ -0,0 +1,144 @@
+let data = [
+  {
+    type: 'realEstate',
+    nameTag: 'fridge',
+    parentFilter: 'kitchen'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'microwave',
+    parentFilter: 'kitchen'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'oven',
+    parentFilter: 'kitchen'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'cookerHood',
+    parentFilter: 'kitchen'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'builtinLighting',
+    parentFilter: 'kitchen'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'tv-livingroom',
+    parentFilter: 'livingroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'furnitures',
+    parentFilter: 'livingroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'eatingTable',
+    parentFilter: 'livingroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'plants',
+    parentFilter: 'livingroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'firePlace',
+    parentFilter: 'livingroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'bathlab',
+    parentFilter: 'bathroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'separatedShower',
+    parentFilter: 'bathroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'washingMachine',
+    parentFilter: 'bathroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'heatedFloor',
+    parentFilter: 'bathroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'tv-bedroom',
+    parentFilter: 'bedroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'builtinCloset',
+    parentFilter: 'bedroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'closet',
+    parentFilter: 'bedroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'bed',
+    parentFilter: 'bedroom'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'balcony',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'cleaningRoom',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'sauna',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'gym',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'freeParking',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'cinema',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'basement',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'garrage',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'yard',
+    parentFilter: 'otherExtras'
+  },
+  {
+    type: 'realEstate',
+    nameTag: 'pool',
+    parentFilter: 'otherExtras'
+  },
+]
+
+module.exports = data
