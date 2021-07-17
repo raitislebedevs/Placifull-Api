47e1d10a3e42469a1ee3f37a3e4e1d552387c866
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-07-17 18:30:11 +0300

    Stripe payment added

---
+++ api/payment/controllers/payment.js
@@ -0,0 +1,32 @@
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51HYshtLQeXlfqG3xi3WD3orit2IZ1YbsbGb9m7ZaxED2jLuP2Pjxylts039Hj1S5wCnOztry7RnTzY79Wfq0nEFE00vSvuvES3"
+);
+
+module.exports = {
+  async chargeCard(ctx) {
+    try {
+      const { amount, source, receipt_email, description } = ctx.request.body;
+
+      const charge = await stripe.paymentIntents.create({
+        amount,
+        currency: "eur",
+        payment_method: source,
+        receipt_email,
+        description,
+        confirm: true,
+      });
+
+      if (!charge) throw new Error("charge unsuccessful");
+
+      ctx.send({
+        message: "charge posted successfully",
+        charge,
+      });
+    } catch (error) {
+      ctx.send({
+        message: error.message,
+      });
+    }
+  },
+};
