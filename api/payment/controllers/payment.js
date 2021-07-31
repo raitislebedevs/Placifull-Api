fe9c4484e82f84b630f7895ca3f8c7021368a4cd
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-07-31 11:43:59 +0300

    Environment Update

--- api/payment/controllers/payment.js
+++ api/payment/controllers/payment.js
@@ -1,6 +1,9 @@
 "use strict";
+// const stripe = require("stripe")(
+//   "sk_test_51HYshtLQeXlfqG3xi3WD3orit2IZ1YbsbGb9m7ZaxED2jLuP2Pjxylts039Hj1S5wCnOztry7RnTzY79Wfq0nEFE00vSvuvES3"
+// );
 const stripe = require("stripe")(
-  "sk_test_51HYshtLQeXlfqG3xi3WD3orit2IZ1YbsbGb9m7ZaxED2jLuP2Pjxylts039Hj1S5wCnOztry7RnTzY79Wfq0nEFE00vSvuvES3"
+  "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"
 );
 
 module.exports = {
